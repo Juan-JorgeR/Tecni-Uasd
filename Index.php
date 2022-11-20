3ccd7c25307c9f5bc4308b938d27ea5449a8ce71
Juan Jorge <juanjorge414@gmail.com>
2022-11-20 15:50:48 -0400

    version inicial

---
+++ Index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Tecni-uasd</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header id="encabezado">
+            <nav id="barraSuperior">
+                <img id="tres_rayas" src="./images/tres_rayas.png">
+                <label id="tituloBarraSuperior">Tecni-uasd</label>
+            </nav>
+        </header>
+        <main id="main">
+            <aside id="menuOpciones">
+                <ul class="opciones_menu" id="opc_menu">
+                    <li class="opcion" id="opc_consultas">
+                        <img src="./images/triangulo.png">
+                        <label>Consultas</label>
+                    </li>
+                    <ul class="opciones_menu" id="sub_opc_consultas">
+                        <li class="opcion" id="opc_descripcion_de_los_Equipos">
+                            <label>Descripcion de los equipos</label>
+                        </li>
+                        <li class="opcion" id="opc_estatus_cuentas_por_cobrar_a_credito">
+                            <label>Estatus cuentas por cobrar a credito</label>
+                        </li>
+                        <li class="opcion" id="opc_existencia_de_garantia">
+                            <label>Existencia de garantia</label>
+                        </li>
+                        <li class="opcion" id="opc_garantia_por_cliente">
+                            <label>garantia por clientes</label>
+                        </li>
+                        <li class="opcion" id="opc_informacion_tecnica_del_producto">
+                            <label>Informacion tecnica del producto</label>
+                        </li>
+                    </ul>
+                    <li class="opcion" id="opc_informes">
+                        <img src="./images/triangulo.png">
+                        <label>Informes</label>
+                    </li>
+                    <ul class="opciones_menu" id="sub_opc_informes">
+                        <li class="opcion" id="opc_informe_equipos_vendidos_por_vendedor">
+                            <label>Informe equipos vendidos por vendedor</label>
+                        </li>
+                        <li class="opcion" id="opc_informe_facturas_mensuales">
+                            <label>Informe facturas mensuales</label>
+                        </li>
+                        <li class="opcion" id="opc_informe_ventas_al_contado">
+                            <label>Informes ventas al contado</label>
+                        </li>
+                        <li class="opcion" id="opc_informe_ventas_a_credito">
+                            <label>Informe ventas a credito</label>
+                        </li>
+                    </ul>
+                    <li class="opcion" id="opc_formularios">
+                        <img src="./images/triangulo.png">
+                        <label>Formularios</label>
+                    </li>
+                    <ul class="opciones_menu" id="sub_opc_formularios">
+                        <li class="opcion" id="opc_cliente">
+                            <label>Cliente</label>
+                        </li>
+                        <li class="opcion" id="opc_factura">
+                            <label>Factura</label>
+                        </li>
+                        <li class="opcion" id="opc_garantia">
+                            <label>Garantia</label>
+                        </li>
+                        <li class="opcion" id="opc_vendedor">
+                            <label>Vendedor</label>
+                        </li>
+                        <li class="opcion" id="opc_marca">
+                            <label>Marca</label>
+                        </li>
+                        <li class="opcion" id="opc_equipo">
+                            <label>Equipos</label>
+                        </li>
+                        <li class="opcion" id="opc_proveedor">
+                            <label>Proveedor</label>
+                        </li>
+                    </ul>
+                </ul>
+            </aside>
+            <section>
+                <?php
+
+                ?>
+            </section>
+        </main>
+    </body>
+    <script src="./js/script.js"></script>
+</html>
